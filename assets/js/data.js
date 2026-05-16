bd7afacf95df4c103a1ab3a7194c5645229b054b
s41mu <262785702+s41mu@users.noreply.github.com>
2026-05-16 17:34:26 +0900

    data: ワールドを新しい順にした

--- assets/js/data.js
+++ assets/js/data.js
@@ -1,106 +1,6 @@
 window.siteData = {
   worlds: [
     {
-      name: "Ψ",
-      poems: [
-        {
-          font: "Imitate",
-          body: `隕九▽繧√※縺?◆縺ｮ縺九?
-豐｢螻ｱ縺ｮ辟｡讖溯ｳｪ縺後?
-
-蜈ｨ縺ｦ繧上◆縺上＠縺ｮ謇?轤ｺ縺ｧ縺吶?
-逎ｨ繧顔｡晏ｭ舌?螂･縺ｫ諞ｧ繧後◆縲
-縺?▽縺九?豸医∴螟ｱ縺帙※縲
-繧ｶ繝ｩ繝｡縺ｮ繧医≧縺ｪ遨ｺ諠ｳ繧ゅ?
-
-縺ゅ↑縺溘ｂ豸医∴縺ｦ蠕?縺上?縺ｧ縺吶?
-髮ｨ髻ｳ縺ｫ諤ｯ縺域釜繧後◆蛯倡ｪ√″蜃ｺ縺励?
-逅?ｸ榊ｰｽ縺ｫ謾ｻ謦?＆繧後◆縺ｮ縺ｧ縲
-蠖｢縺ｮ蜷医ｏ縺ｪ縺?ｨｲ闍励?縲`,
-          origin: [
-            {
-              name: "◘",
-              link: "https://lyrics.imicomweb.com/songs/4/",
-            },
-            {
-              name: ".",
-              link: "https://lyrics.imicomweb.com/songs/5/"
-            },
-            {
-              name: "。",
-              link: "https://lyrics.imicomweb.com/songs/60/",
-            },
-            {
-              name: "．",
-              link: "https://lyrics.imicomweb.com/songs/811/"
-            },
-            {
-              name: "■■",
-              link: "https://lyrics.imicomweb.com/songs/1004/"
-            },
-            {
-              name: "K²",
-              link: "https://lyrics.imicomweb.com/songs/18/"
-            }
-          ]
-        }
-      ]
-    },
-    {
-      name: "ψ",
-      poems: [
-        {
-          font: "Imitate",
-          body: `形の合わない稲苗が、
-摩天楼に囚われ殺された。
-骨を折る雨音に去るのでしょう、
-あなたも流れ往くのです。
-
-宙を見つめた頭蓋の中、
-目が覚め消え失せて、
-黄金色漏れる硝子の中、
-全てが債務な所為で。
-
-沢山の白い箱が、
-見つめていたのか。`,
-          origin: [
-            {
-              name: ".",
-              link: "https://lyrics.imicomweb.com/songs/5/"
-            },
-            {
-              name: "。",
-              link: "https://lyrics.imicomweb.com/songs/60/",
-            },
-            {
-              name: "．",
-              link: "https://lyrics.imicomweb.com/songs/811/"
-            },
-            {
-              name: ".谵语ᚋ",
-              link: "https://lyrics.imicomweb.com/songs/595/"
-            },
-            {
-              name: "ゆめをくいつくすバクたちは",
-              link: "https://lyrics.imicomweb.com/songs/5963/"
-            },
-            {
-              name: "インサイド",
-              link: "https://lyrics.imicomweb.com/songs/116/"
-            },
-            {
-              name: "■■",
-              link: "https://lyrics.imicomweb.com/songs/1004/"
-            },
-            {
-              name: "K²",
-              link: "https://lyrics.imicomweb.com/songs/18/"
-            }
-          ]
-        }
-      ]
-    },
-    {
       name: "φ",
       poems: [
         {
@@ -171,6 +71,106 @@ window.siteData = {
             {
               name: "ルミエール",
               link: "https://lyrics.imicomweb.com/songs/257/"
+            }
+          ]
+        }
+      ]
+    },
+    {
+      name: "ψ",
+      poems: [
+        {
+          font: "Imitate",
+          body: `形の合わない稲苗が、
+摩天楼に囚われ殺された。
+骨を折る雨音に去るのでしょう、
+あなたも流れ往くのです。
+
+宙を見つめた頭蓋の中、
+目が覚め消え失せて、
+黄金色漏れる硝子の中、
+全てが債務な所為で。
+
+沢山の白い箱が、
+見つめていたのか。`,
+          origin: [
+            {
+              name: ".",
+              link: "https://lyrics.imicomweb.com/songs/5/"
+            },
+            {
+              name: "。",
+              link: "https://lyrics.imicomweb.com/songs/60/",
+            },
+            {
+              name: "．",
+              link: "https://lyrics.imicomweb.com/songs/811/"
+            },
+            {
+              name: ".谵语ᚋ",
+              link: "https://lyrics.imicomweb.com/songs/595/"
+            },
+            {
+              name: "ゆめをくいつくすバクたちは",
+              link: "https://lyrics.imicomweb.com/songs/5963/"
+            },
+            {
+              name: "インサイド",
+              link: "https://lyrics.imicomweb.com/songs/116/"
+            },
+            {
+              name: "■■",
+              link: "https://lyrics.imicomweb.com/songs/1004/"
+            },
+            {
+              name: "K²",
+              link: "https://lyrics.imicomweb.com/songs/18/"
+            }
+          ]
+        }
+      ]
+    },
+    {
+      name: "Ψ",
+      poems: [
+        {
+          font: "Imitate",
+          body: `隕九▽繧√※縺?◆縺ｮ縺九?
+豐｢螻ｱ縺ｮ辟｡讖溯ｳｪ縺後?
+
+蜈ｨ縺ｦ繧上◆縺上＠縺ｮ謇?轤ｺ縺ｧ縺吶?
+逎ｨ繧顔｡晏ｭ舌?螂･縺ｫ諞ｧ繧後◆縲
+縺?▽縺九?豸医∴螟ｱ縺帙※縲
+繧ｶ繝ｩ繝｡縺ｮ繧医≧縺ｪ遨ｺ諠ｳ繧ゅ?
+
+縺ゅ↑縺溘ｂ豸医∴縺ｦ蠕?縺上?縺ｧ縺吶?
+髮ｨ髻ｳ縺ｫ諤ｯ縺域釜繧後◆蛯倡ｪ√″蜃ｺ縺励?
+逅?ｸ榊ｰｽ縺ｫ謾ｻ謦?＆繧後◆縺ｮ縺ｧ縲
+蠖｢縺ｮ蜷医ｏ縺ｪ縺?ｨｲ闍励?縲`,
+          origin: [
+            {
+              name: "◘",
+              link: "https://lyrics.imicomweb.com/songs/4/",
+            },
+            {
+              name: ".",
+              link: "https://lyrics.imicomweb.com/songs/5/"
+            },
+            {
+              name: "。",
+              link: "https://lyrics.imicomweb.com/songs/60/",
+            },
+            {
+              name: "．",
+              link: "https://lyrics.imicomweb.com/songs/811/"
+            },
+            {
+              name: "■■",
+              link: "https://lyrics.imicomweb.com/songs/1004/"
+            },
+            {
+              name: "K²",
+              link: "https://lyrics.imicomweb.com/songs/18/"
             }
           ]
         }
